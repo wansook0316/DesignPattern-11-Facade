8dbb24d3cfc7b16a1912c6a13eba92884bad56fc
wansook0316 <wansook0316@gmail.com>
2022-09-20 09:45:48 +0900

    Update: Row

---
+++ Facade/Row.swift
@@ -0,0 +1,14 @@
+//
+//  Row.swift
+//  Facade
+//
+//  Created by Choiwansik on 2022/09/20.
+//
+
+import Foundation
+
+internal struct Row {
+    private(set) var name: String
+    private(set) var birthday: String
+    private(set) var email: String
+}
