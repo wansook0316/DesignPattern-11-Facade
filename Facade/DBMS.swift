fc41a4b219c0a97c5fda97b2583ceabd5512020f
wansook0316 <wansook0316@gmail.com>
2022-09-20 09:52:55 +0900

    Update: DBMS

---
+++ Facade/DBMS.swift
@@ -0,0 +1,25 @@
+//
+//  DBMS.swift
+//  Facade
+//
+//  Created by Choiwansik on 2022/09/20.
+//
+
+import Foundation
+
+internal struct DBMS {
+
+    internal init() {
+        self.dataBase["wansik"] = Row(name: "wansik", birthday: "03.16", email: "wansook0316@gmail.com")
+        self.dataBase["hyejin"] = Row(name: "hyejin", birthday: "11.16", email: "hyejin@gmail.com")
+        self.dataBase["jungmoo"] = Row(name: "jungmoo", birthday: "05.15", email: "jungmoo@gmail.com")
+    }
+
+    internal func query(name: String) -> Row? {
+        Thread.sleep(forTimeInterval: 1000)
+
+        return self.dataBase[name]
+    }
+
+    private var dataBase = [String: Row]()
+}
